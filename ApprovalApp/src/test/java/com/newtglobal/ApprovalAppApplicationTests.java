e6ec0b134baabaec336d064886888ba0b8a7cc5e
divyanewt <divikvcet95@gmail.com>
2017-05-31 15:11:33 +0530

    Update ApprovalAppApplicationTests.java

--- ApprovalApp/src/test/java/com/newtglobal/ApprovalAppApplicationTests.java
+++ ApprovalApp/src/test/java/com/newtglobal/ApprovalAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.newtglobal;
+/*package com.newtglobal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,4 +13,4 @@ public class ApprovalAppApplicationTests {
 	public void contextLoads() {
 	}
 
-}
+}*/
